8c4aac4b36dc2e6f568ba6cca6bcb794056498df
Nikita Morozov <nikmorze@gmail.com>
2019-09-26 16:03:57 +0400

    new packages and settings

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,33 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-require("@babel/register");
+require('@babel/register');
 
 const config = {
-  entry: ['@babel/polyfill','./src/index.js'],
+  entry: ['@babel/polyfill', './src/index.js'],
   output: {
     path: __dirname + '/public',
-    filename: 'bundle.js'
+    filename: 'bundle.js',
   },
   module: {
-    rules : [
+    rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: ['babel-loader']
+        use: ['babel-loader'],
       },
       {
         test: /\.s[ac]ss$/i,
-        use: [
-          'style-loader',
-          'css-loader',
-          'sass-loader',
-        ],
+        use: ['style-loader', 'css-loader', 'sass-loader'],
       },
-    ]
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-        hash: true
-    })
+      hash: true,
+    }),
   ],
   resolve: {
-    modules: [
-      path.resolve('./src'),
-      path.resolve('./node_modules')
-    ]
+    modules: [path.resolve('./src'), path.resolve('./node_modules')],
   },
   devServer: {
     contentBase: __dirname + '/public',
@@ -42,17 +35,17 @@ const config = {
     port: 9000,
     open: true,
     stats: {
-        assets: false,
-        children: false,
-        chunks: false,
-        chunkModules: false,
-        colors: true,
-        entrypoints: false,
-        hash: false,
-        modules: false,
-        timings: false,
-        version: false,
-    }
+      assets: false,
+      children: false,
+      chunks: false,
+      chunkModules: false,
+      colors: true,
+      entrypoints: false,
+      hash: false,
+      modules: false,
+      timings: false,
+      version: false,
+    },
   },
   watch: false,
   devtool: 'source-map',
